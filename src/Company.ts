f46c1324d7283846c3c39ad3b62cedad69f6e85f
aroran1 <nidhigulati2001@gmail.com>
2020-11-29 10:37:35 +0000

    implements

--- src/Company.ts
+++ src/Company.ts
@@ -1,12 +1,14 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
-  }
+  };
+  color: string = "blue";
 
   constructor() {
     this.companyName = faker.company.companyName(),
--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,12 +1,13 @@
 import { User } from './User';
 import { Company } from './Company';
 
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
   markerContent: () => string;
+  color: string;
 }
 
 
@@ -34,7 +35,7 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: "Hi there!"
+        content: mappable.markerContent()
       });
 
       infoWindow.open(this.googleMap, marker);
